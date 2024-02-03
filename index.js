7ce5fc41817a455fb33f732aa2441b0ae77d41a3
nguyenquoctai872@gmail.com <115157841+QuocTai2002@users.noreply.github.com>
2024-02-03 14:42:52 +0700

    fix: fix cors localhost:3000

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const express = require('express')
 const app = express()
 const webRouter = require('./src/routes/web')
 const initAPIRoute = require('./src/routes/api')
+const cors = require('cors')
 const port = process.env.PORT || 8080 // port => hard code
 // const hostname = process.env.HOST_NAME
-
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
 // config req.body
 app.use(express.json());
 // app.use(express.urlencoded({extended: true}));
-
+// access cross
+app.use(cors(corsOptions));
  // route API
  initAPIRoute(app)
  // khai bao route
